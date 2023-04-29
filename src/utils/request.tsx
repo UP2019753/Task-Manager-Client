1e40cf306efc23e5cc529fdff77142cad37f98bf
UP2019753 <91560784+UP2019753@users.noreply.github.com>
2023-04-29 22:21:48 +0100

    fix request url

--- src/utils/request.tsx
+++ src/utils/request.tsx
@@ -1,3 +1,3 @@
 import { GraphQLClient } from "graphql-request";
 
-export const graphQLClient = new GraphQLClient(import.meta.env.API_URL);
+export const graphQLClient = new GraphQLClient(import.meta.env.VITE_API_URL);
